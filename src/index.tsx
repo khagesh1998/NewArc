8ff224354e9cd649eafcef1ebf9c52c57c8f574d
khagesh1998 <khagesh.kumar@dream11.com>
2022-09-12 20:06:18 +0530

    chore: jni working condition for turbo modules

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export const MainActivity = () => {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+};
